5de6130ebf682e5ba0d1bbb8fac015c12dbe3434
Alexander <alexander.veselov@spryker.com>
2019-09-26 09:57:22 +0000

    MP-716: MP-37: Created statuses & expander plugins for merchant table

--- src/Spryker/Zed/MerchantProfile/Business/MerchantProfileFacadeInterface.php
+++ src/Spryker/Zed/MerchantProfile/Business/MerchantProfileFacadeInterface.php
@@ -7,6 +7,8 @@
 
 namespace Spryker\Zed\MerchantProfile\Business;
 
+use Generated\Shared\Transfer\MerchantProfileCriteriaFilterTransfer;
+use Generated\Shared\Transfer\MerchantProfileTransfer;
 use Generated\Shared\Transfer\MerchantTransfer;
 
 interface MerchantProfileFacadeInterface
@@ -22,4 +24,16 @@ interface MerchantProfileFacadeInterface
      * @return \Generated\Shared\Transfer\MerchantTransfer
      */
     public function saveMerchantProfile(MerchantTransfer $merchantTransfer): MerchantTransfer;
+
+    /**
+     * Specification:
+     * - Finds merchant profile by Criteria.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\MerchantProfileCriteriaFilterTransfer $merchantProfileCriteriaFilterTransfer
+     *
+     * @return \Generated\Shared\Transfer\MerchantProfileTransfer|null
+     */
+    public function findOne(MerchantProfileCriteriaFilterTransfer $merchantProfileCriteriaFilterTransfer): ?MerchantProfileTransfer;
 }
